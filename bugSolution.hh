03d093f42c1bec5e09f4115ed59d719bf01578f2
BugHunter-X <askzenai@gmail.com>
2025-01-19 00:30:06 +0100

    Add bugSolution.hh file

---
+++ bugSolution.hh
@@ -0,0 +1,12 @@
+function foo(x: int): int {
+  var result: int = 1;
+  for (var i = 1; i <= x; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+function main(): void {
+  echo foo(5);
+}
+This iterative version of the function calculates the factorial without the risk of a stack overflow.
